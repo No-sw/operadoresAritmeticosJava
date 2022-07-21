deb6476dbeda7b3a19dfc5f8290f6676f45a8f06
Owen de Jesus <owendanieldejesuslopez70@gmail.com>
2022-07-20 21:04:40 -0600

    Agregue un Hola Mundo

--- src/operacionesaritmeticas/OperacionesAritmeticas.java
+++ src/operacionesaritmeticas/OperacionesAritmeticas.java
@@ -15,6 +15,6 @@ public class OperacionesAritmeticas {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("¡Hola Mundo!");
     }
-    
 }
